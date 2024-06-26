93358584d69fa092654f1aec0503b2b197b7c368
YO!CHI KIKUCHI <i@yo1000.com>
2024-06-26 17:19:14 +0900

    Improve styles

--- spring-nextjs-server/src/main/resources/frontend/components/Table.tsx
+++ spring-nextjs-server/src/main/resources/frontend/components/Table.tsx
@@ -43,8 +43,8 @@ export default function Table<T>({data, searchLabel = "Keyword", onSearch, onAdd
                                    type="text"
                                    name="keyword"
                                    id="keyword"
-                                   className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                   placeholder="Keyword"/>
+                                   className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                   placeholder="Forward matching keyword"/>
                         </div>
                     </div>
                     <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-none">
